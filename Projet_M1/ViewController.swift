9f460db932b7a64c5fba8e39d3d4afadaf35781c
Manuel <mandu543@gmail.com>
2023-04-05 13:44:33 +0200

    ajout liaison viewcontroller

--- Projet_M1/ViewController.swift
+++ Projet_M1/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageBot: UILabel!
+    @IBOutlet weak var nombreSasie: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func valideChoice(_ sender: UIButton) {
+    }
+    
+    
+    
 
 }
 
